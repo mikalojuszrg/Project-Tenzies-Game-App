2ba65977e36bbecbce76896b3df882dfae8fe489
mikalojuszrg <mikalojus.zirgulis@gmail.com>
2023-01-22 16:03:25 +0200

    first functionaities added

--- src/components/Die/Die.jsx
+++ src/components/Die/Die.jsx
@@ -3,8 +3,18 @@ import classNames from "classnames";
 
 const cn = classNames.bind(styles);
 
-const Die = ({ value }) => {
-  return <div className={cn(styles.die)}>{value}</div>;
+const Die = ({ value, isHeld }) => {
+  return (
+    <div
+      className={
+        isHeld
+          ? cn(styles.die, styles["die--active"])
+          : cn(styles.die, styles["die--inactive"])
+      }
+    >
+      {value}
+    </div>
+  );
 };
 
 export default Die;
--- src/components/GameBoard/GameBoard.jsx
+++ src/components/GameBoard/GameBoard.jsx
@@ -1,25 +1,40 @@
 import styles from "./GameBoard.module.scss";
+import { nanoid } from "nanoid";
 import classNames from "classnames";
 import Die from "../Die/Die";
+import { useState } from "react";
 
 const cn = classNames.bind(styles);
 
+const allNewDice = () => {
+  const array = Array(10)
+    .fill()
+    .map(() => ({
+      value: Math.ceil(Math.random() * 6),
+      isHeld: false,
+      id: nanoid(),
+    }));
+  return array;
+};
+
 const GameBoard = () => {
+  const [diceNumbers, setDiceNumbers] = useState(() => allNewDice());
+
+  const handleDiceNumbers = () => {
+    setDiceNumbers(allNewDice());
+  };
+
   return (
     <main className={cn(styles.game)}>
       <div className={cn(styles.game__board)}>
         <div className={cn(styles.game__dices)}>
-          <Die value="1"></Die>
-          <Die value="2"></Die>
-          <Die value="2"></Die>
-          <Die value="2"></Die>
-          <Die value="3"></Die>
-          <Die value="3"></Die>
-          <Die value="3"></Die>
-          <Die value="4"></Die>
-          <Die value="5"></Die>
-          <Die value="6"></Die>
+          {diceNumbers.map((number) => (
+            <Die isHeld={number.isHeld} key={number.id} value={number.value} />
+          ))}
         </div>
+        <button onClick={handleDiceNumbers} className={cn(styles.game__button)}>
+          Roll
+        </button>
       </div>
     </main>
   );
